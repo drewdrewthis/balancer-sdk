4648c7c783c2281e1c491deae1cf4f18ae5224b7
drewdrew <drewdrewthis@gmail.com>
2022-05-06 14:44:23 +0200

    Start working on weth method for vault

--- balancer-rs/tests/vault_integration_spec.rs
+++ balancer-rs/tests/vault_integration_spec.rs
@@ -11,4 +11,12 @@ mod tests {
 
     assert_eq!(result, 1);
   }
+
+  // Test to see if the module can get the WETH address from the vault
+  #[test]
+  fn test_weth() {
+    let result = balancer_rs::vault::weth();
+
+    assert_eq!(result, "test");
+  }
 }
