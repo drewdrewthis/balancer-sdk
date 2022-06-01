8b924fea8d07df9d0db2e4f3a78c7cbeb00c625b
drewdrew <drewdrewthis@gmail.com>
2022-06-01 12:00:40 +0200

    Add test macro for pools

--- balancer-rs/src/pools/mod.rs
+++ balancer-rs/src/pools/mod.rs
@@ -31,3 +31,33 @@ define_contract!(ManagedPool, GeneratedManagedPool);
 
 // The API for the MetaStablePool will work for this contract. We don't have a JSON ABI for it for now.
 define_contract!(WeightedPool2Tokens, GeneratedMetaStablePool);
+
+#[cfg(test)]
+pub mod tests {
+  // Note this useful idiom: importing names from outer (for mod tests) scope.
+  use super::*;
+  use std::str::FromStr;
+
+  macro_rules! test_pool_instatiation {
+    ($pool:tt) => {{
+      const RPC_URL: &'static str = "https://rpc.flashbots.net/";
+      const POOL_ADDRESS: &'static str = "0x01abc00e86c7e258823b9a055fd62ca6cf61a163";
+
+      let transport = ethcontract::web3::transports::Http::new(RPC_URL).unwrap();
+      let web3 = ethcontract::Web3::new(transport);
+      let pool_address = crate::addr!(POOL_ADDRESS);
+
+      $pool::new(web3, pool_address);
+    }};
+  }
+
+  #[test]
+  fn test_instantiate_pools() {
+    test_pool_instatiation!(WeightedPool);
+    test_pool_instatiation!(WeightedPool2Tokens);
+    test_pool_instatiation!(LiquidityBootStrappingPool);
+    test_pool_instatiation!(ManagedPool);
+    test_pool_instatiation!(StablePool);
+    test_pool_instatiation!(MetaStablePool);
+  }
+}
